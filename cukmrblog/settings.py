dd2ef451c5b8beaaaa0f2d587e938033f8b420bb
shyyashowkat <shyxum96@gmail.com>
2020-12-29 20:02:02 +0530

    test

--- cukmrblog/settings.py
+++ cukmrblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's%diu0z+hkv(x)kb@td^^(93x+#-tu9v5cd1x7#yf5d*o2n)9p'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
